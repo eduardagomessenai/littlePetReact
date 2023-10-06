93b62584fc7f8e22dc82180e40090d1c4726f7cc
dudsssenai <eduardagomesdesouza8@gmail.com>
2023-10-06 17:06:28 -0300

    Inicio do projeto LittlePet.

---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,26 @@
+import styles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+
+      </div>
+
+      <input type="text"/>
+
+      <div className={styles.login}>
+
+      </div>
+
+      <div className={styles.social}>
+
+      </div>
+
+
+
+    </div>
+  );
+};
+
+export default Header;
