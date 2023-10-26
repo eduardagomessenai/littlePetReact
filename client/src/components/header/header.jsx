0966bb9ff50c75bbddd952cb4e144da6fc4f1c9a
dudsssenai <eduardagomesdesouza8@gmail.com>
2023-10-26 17:03:06 -0300

    Inicio do header - logo.

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,23 +1,37 @@
 import styles from "./header.module.css";
 
+import patinha from "../../assets/patinha.svg";
+import search from "../../assets/search.svg";
+import people from "../../assets/people.svg";
+import heart from "../../assets/heart.svg";
+import bag from "../../assets/bag.svg";
+
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>
 
+      <div className={styles.logo}>
+        Little Pet
+        <img src={patinha} alt="" />
       </div>
 
-      <input type="text"/>
+      <div>
+        <input type="text" />
+        <img src={search} alt="" />
+      </div>
 
       <div className={styles.login}>
-
+        <img src={people} alt="" />
+        <div>
+          <p>Entrar</p>
+          <p>Cadastrar</p>
+        </div>
       </div>
 
       <div className={styles.social}>
-
+        <img src={heart} alt="" />
+        <img src={bag} alt="" />
       </div>
-
-
 
     </div>
   );
